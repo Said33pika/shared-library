d67abb87270123c1eae186dccf95fad5216abc18
Said33pika <44300933+Said33pika@users.noreply.github.com>
2021-02-08 18:16:10 +0530

    Update logs.groovy

--- vars/logs.groovy
+++ vars/logs.groovy
@@ -8,4 +8,8 @@ def call(String filter_string, int occurrence) {
     if (count > occurrence -1) {
         currentBuild.result='UNSTABLE'
     }
+    else
+    {
+        currentBuild.result='STABLE'
+    }
 }
