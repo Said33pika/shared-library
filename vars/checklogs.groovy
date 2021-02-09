34a8721a00919a7ba9a8937af7cf41196ba93e97
Said33pika <44300933+Said33pika@users.noreply.github.com>
2021-02-09 10:10:24 +0530

    Update checklogs.groovy

--- vars/checklogs.groovy
+++ vars/checklogs.groovy
@@ -5,8 +5,8 @@ import org.apache.commons.lang.StringUtils
 def call(String filter_string, int occurrence) {
     def logs = currentBuild.rawBuild.getLog(10000).join('\n')
     int count = StringUtils.countMatches(logs, filter_string);
-    println(count)
-    println(occurrence)
+    //println(count)
+    //println(occurrence)
     if (count > occurrence - 1) {
         currentBuild.result = 'UNSTABLE'
     }
